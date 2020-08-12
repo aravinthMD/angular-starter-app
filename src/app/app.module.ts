9195821977f6526a4524b01b8e36d077d9672021
Aravinth <aravinth.muthusamy@appiyo.com>
2020-08-12 17:35:04 +0530

    Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {AppRoutingModule} from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NewDetailComponent } from './new-detail/new-detail.component';
+import { ListComponent } from './list/list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewDetailComponent,
+    ListComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
